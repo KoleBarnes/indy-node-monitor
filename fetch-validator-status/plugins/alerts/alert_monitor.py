364504721c6c47855fae17dfcf2d7bd8e733f501
KoleBarnes <kbarnes261@gmail.com>
2021-02-23 13:58:23 -0800

    WIP: Status working
    Redid how the Node contact info is retrieved from Airtable. Now is own function inside the alerts plug-in.
    Made the creation of an alert log its own function inside the alerts plug-in.
    Displaying the recipients email in the email contents until it can be trusted to email automatically.
    
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- fetch-validator-status/plugins/alerts/alert_monitor.py
+++ fetch-validator-status/plugins/alerts/alert_monitor.py
@@ -41,78 +41,24 @@ class main(plugin_collection.Plugin):
             # print(json.dumps(result, indent=2))
 
             if result: 
-                
-                page = 0
-                AIRTABLE_API_KEY = os.environ.get('Airtable_API_Key')
-                params = {}
-                headers = {"Authorization": "Bearer " + AIRTABLE_API_KEY}
-                #Companies_url = "https://api.airtable.com/v0/appq6dtcBJNmgbmCt/Companies"
-                Steward_Nodes_url = "https://api.airtable.com/v0/appq6dtcBJNmgbmCt/Steward%20Nodes"
-                Contacts_url = "https://api.airtable.com/v0/appq6dtcBJNmgbmCt/Contacts"
-
-                Steward_Nodes_res = requests.get( Steward_Nodes_url, headers=headers).json()
-                #print(json.dumps(Steward_Nodes_res, indent=2))
-
                 for node in result:
 
                     ''' 
                     - Archive alert files when there aren't alerts/alerts have been resolved
+                    
+                    - get rid of email sent and use time sent if its not null then the was sent
+                    
+                    - Take out Email content from there individual files and just have them all in one file
+
+                    - call all stages feilds steps in the json file
+                    - pop node from the data pulled from the json file once the data has been colected then use
+                      time till email to sort the steps with (key=lambda x: x.index, reverse=False) then
+                      go throught the steps to see when and if an email is to be sent.
                     '''
 
                     node_name = node["name"]
                     alert_log_path = "./plugins/alerts/AlertLogs/"
                     ALL_EMAILS_SENT = False
-                    
-                    # Take the network_name and the node_name of the node that is down and get the company ID (airtable company instance ID) from airtable
-                    for piece in Steward_Nodes_res["records"]:
-                        if piece["fields"]["Network"] == network_name:
-                            if "Nodes" in piece["fields"] or "Company" in piece["fields"]:
-                                if piece["fields"]["Nodes"] == node_name:
-                                    AirTable_Company_ID = piece["fields"]["Company"]
-                                    print(f"company ID: {AirTable_Company_ID} Node Name: {node_name}\n")
-
-                    # Go through evey contact and find the contacts that have the matching company ID (airtable company instance ID). Get their contact types and see if they are the tachnical contact and get their info.
-                    hey = True
-                    while hey:
-                        Contacts_res = requests.get(Contacts_url, params=params, headers=headers).json()
-                        #print(json.dumps(Contacts_res, indent=2))
-
-                        for piece in Contacts_res["records"]:
-                            if piece["fields"]["Company"] == AirTable_Company_ID:
-                                if "Contact Type" in piece["fields"]:
-                                    Contact_Type = piece["fields"]["Contact Type"]
-                                else:
-                                    Email = None
-
-                                if "Technical" in Contact_Type: 
-                                
-                                    if "First Name" in piece["fields"]:
-                                        First_Name = piece["fields"]["First Name"]
-                                    else:
-                                        First_Name = None
-
-                                    if "Last Name" in piece["fields"]:
-                                        Last_Name = piece["fields"]["Last Name"]
-                                    else:
-                                        Last_Name = None
-
-                                    if "Email" in piece["fields"]:
-                                        Email = piece["fields"]["Email"]
-                                    else:
-                                        Email = None
-
-                                    print(f"\nName: {First_Name} {Last_Name}\nContact Type: {Contact_Type}\nEmail: {Email}\n")
-
-                        # Pagination
-                        # The airtable server returns one page of records at a time. Each page will contain 100 records by default.
-                        # If there are more records, the response will contain an offset. We then use the offset in params to get the next page of records.
-                        # Pagination will stop when you've reached the end of the table.
-                        page += 1
-                        if "offset" in Contacts_res:
-                            params["offset"] = Contacts_res["offset"]
-                        else:
-                            params["offset"] = None
-                            hey = False
 
                     # If Alert already exist work with it; else create an Alert
                     if os.path.exists(f'{alert_log_path}{node_name}.json'):
@@ -174,30 +120,14 @@ class main(plugin_collection.Plugin):
 
                     # Create a file for the new alert if there isn't one.
                     else:
-                        print('New alert creating log...')
-
-                        email_content_path = "./plugins/alerts/EmailContent/"
-                        recipients_email = ["kole@sovrin.org"]
-                        cc_email = [None]
-
-                        alert = {
-                            "node": node,
-                            "stageOne": {"email_sent": False, "time_sent": None, "time_till_email": 120, "HTML_content": email_content_path + "StageOne/StageOneEmail.html", "plainText_content": email_content_path + "StageOne/StageOneEmail.txt", "recipients_email": recipients_email, 'cc_email': cc_email},
-                            "stageTwo": {"email_sent": False, "time_sent": None, "time_till_email": 1440, "HTML_content": email_content_path + "StageTwo/StageTwoEmail.html", "plainText_content": email_content_path + "StageTwo/StageTwoEmail.txt", "recipients_email": recipients_email, 'cc_email': cc_email},
-                            "stageThree": {"email_sent": False, "time_sent": None, "time_till_email": 2880, "HTML_content": email_content_path + "StageThree/StageThreeEmail.html", "plainText_content": email_content_path + "StageThree/StageThreeEmail.txt", "recipients_email": recipients_email, 'cc_email': cc_email}
-                        }
-
-                        # Create Alert file with dict above.
-                        with open(f'{alert_log_path}{node_name}.json', 'w') as outfile:
-                            json.dump(alert, outfile, indent=2)
-                            print('Log created.')
+                        recipients_email = self.get_contact_info(node_name)
+                        self.create_alert_log(node, node_name, alert_log_path, recipients_email)
                         
-
 
     def notify(self, node, network_name, recipients_email, cc_email, html_content, plainText_content):
         EMAIL_ADDRESS = os.environ.get('Sovrin_Email_App_User')
         EMAIL_PASSWORD = os.environ.get('Sorvin_Email_App_Pwd')
-        EMAIL_RECIPIENT = recipients_email
+        EMAIL_RECIPIENT = None
         EMAIL_CC = cc_email
 
         # Find out what network we are on and find which folder the log will be in.
@@ -217,11 +147,11 @@ class main(plugin_collection.Plugin):
         # Email will send a HTML and plain text options for email clients that don't support HTML format.
 
         # Plain Text Option #
-        content = open(plainText_content).read().format(node=node, network_name=network_name, log_folder=log_folder)
+        content = open(plainText_content).read().format(node=node, network_name=network_name, log_folder=log_folder, recipients_email=recipients_email)
         msg.set_content(content)
 
         # HTML Option #
-        html = open(html_content).read().format(node=node, network_name=network_name, log_folder=log_folder)
+        html = open(html_content).read().format(node=node, network_name=network_name, log_folder=log_folder, recipients_email=recipients_email)
         msg.add_alternative(html, subtype='html')
 
         # Send Email with contents and return if the email was sent.
@@ -231,3 +161,54 @@ class main(plugin_collection.Plugin):
             email_sent = True
         
         return email_sent
+
+
+    def get_contact_info(self, node_name):
+
+        AIRTABLE_API_KEY = os.environ.get('Airtable_API_Key')
+        headers = {"Authorization": "Bearer " + AIRTABLE_API_KEY}
+
+        companies_url = "https://api.airtable.com/v0/appq6dtcBJNmgbmCt/Companies/"
+        contacts_url = "https://api.airtable.com/v0/appq6dtcBJNmgbmCt/Contacts/"
+
+        # Gets the list of company contacts associated with the company from airtable
+        companies_url = companies_url + f'?filterByFormula=%7BSteward Nodes%7D="{node_name}"'
+        response = requests.get(companies_url, headers=headers)
+        airtable_response = response.json()
+        # print(json.dumps(airtable_response, indent=2))
+        airtable_records = airtable_response["records"]
+        for record in airtable_records:
+            if "Company Name" in record["fields"]:
+                company_name = record["fields"]["Company Name"]
+                print(company_name)
+        
+        # Gets the technical contacts associated with the company from airtable.
+        contacts_url = contacts_url + f'?filterByFormula=AND(Company="{company_name}", SEARCH("Technical",%7BContact Type copy%7D))'
+        response = requests.get(contacts_url, headers=headers)
+        airtable_response = response.json()
+        # print(json.dumps(airtable_response, indent=2))
+        airtable_records = airtable_response["records"]
+        for record in airtable_records:
+            if "Email" in record["fields"]: Email = record["fields"]["Email"]
+            # print(f"Email: {Email}\n")
+
+        return(Email)
+
+    def create_alert_log(self, node, node_name, alert_log_path, recipients_email):
+        print('New alert creating log...')
+
+        email_content_path = "./plugins/alerts/EmailContent/"
+        recipients_email = [recipients_email]
+        cc_email = ["kole@sovrin.org"]
+
+        alert = {
+            "node": node,
+            "stageOne": {"email_sent": False, "time_sent": None, "time_till_email": 120, "HTML_content": email_content_path + "StageOne/StageOneEmail.html", "plainText_content": email_content_path + "StageOne/StageOneEmail.txt", "recipients_email": recipients_email, 'cc_email': cc_email},
+            "stageTwo": {"email_sent": False, "time_sent": None, "time_till_email": 1440, "HTML_content": email_content_path + "StageTwo/StageTwoEmail.html", "plainText_content": email_content_path + "StageTwo/StageTwoEmail.txt", "recipients_email": recipients_email, 'cc_email': cc_email},
+            "stageThree": {"email_sent": False, "time_sent": None, "time_till_email": 2880, "HTML_content": email_content_path + "StageThree/StageThreeEmail.html", "plainText_content": email_content_path + "StageThree/StageThreeEmail.txt", "recipients_email": recipients_email, 'cc_email': cc_email}
+        }
+
+        # Create Alert file with dict above.
+        with open(f'{alert_log_path}{node_name}.json', 'w') as outfile:
+            json.dump(alert, outfile, indent=2)
+            print('Log created.')
