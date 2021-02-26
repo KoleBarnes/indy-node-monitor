b9afce05506ef6453b26735b7ef93665fcb28801
KoleBarnes <kbarnes261@gmail.com>
2021-02-26 14:28:31 -0800

    WIP: Added error handling for data collection from Airtable. Added Logging. Now an email will be sent if there is an error. Change the required and non-required args for send email. Renamed node to node_name in send_email
    Signed-off-by: KoleBarnes <kbarnes261@gmail.com>

--- fetch-validator-status/plugins/alerts/alert_monitor.py
+++ fetch-validator-status/plugins/alerts/alert_monitor.py
@@ -3,9 +3,10 @@ import datetime
 import argparse
 import os
 import json
+import logging
 import smtplib
-from email.message import EmailMessage
 import requests
+from email.message import EmailMessage
 
 class main(plugin_collection.Plugin):
     
@@ -49,23 +50,30 @@ class main(plugin_collection.Plugin):
                     for node in result:
                         node_name = node["name"]
                         alert_log_path = "./plugins/alerts/AlertLogs/"
-
-                        # self.get_contact_info(node_name)
-                        # exit()
+                        logging.basicConfig(filename='./plugins/alerts/Debug.log', datefmt='%m/%d/%Y %I:%M:%S %p', format='%(asctime)s: %(levelname)s: %(message)s', level=logging.DEBUG)
 
                         if os.path.exists(f'{alert_log_path}{node_name}.json'):
                             self.read_alert_log(node_name, alert_log_path, network_name)
                         else:
-                            recipients_email = self.get_contact_info(node_name)
-                            self.create_alert_log(node, node_name, network_name, alert_log_path, recipients_email)
-                            # self.read_alert_log(node_name, alert_log_path, network_name)
+                            try:
+                                logging.info(f'Getting data for {node_name} on {network_name}')
+                                recipients_email = self.get_contact_info(node_name)
+                            except Exception as e: 
+                                logging.error(e)
+                                print("\033[91mError occurred while getting info from Airtable. Check debug log!\033[m")
+                                EMAIL_ADDRESS = os.environ.get('Sovrin_Email_App_User')
+                                plainText_content = "./plugins/alerts/EmailContent/ErrorAlert.txt"
+                                subject = "Error occurred in Alerts in Node Monitor"
+                                self.send_email(node_name, network_name, plainText_content, cc_email=EMAIL_ADDRESS, subject=subject)
+                            else:
+                                self.create_alert_log(node, node_name, network_name, alert_log_path, recipients_email)
+                                self.read_alert_log(node_name, alert_log_path, network_name)
                 
                 else:
                     print(json.dumps(result, indent=2))
 
                         
     def get_contact_info(self, node_name):
-        EMAIL_ADDRESS = os.environ.get('Sovrin_Email_App_User')
         AIRTABLE_API_KEY = os.environ.get('Airtable_API_Key')
         headers = {"Authorization": "Bearer " + AIRTABLE_API_KEY}
         companies_url = "https://api.airtable.com/v0/appq6dtcBJNmgbmCt/Companies/"
@@ -80,7 +88,8 @@ class main(plugin_collection.Plugin):
         for record in company_records["records"]:
             company_name = record["fields"].get("Company Name")
             if not company_name:
-                print("Could not get company name!")
+                logging.debug(company_records)
+                raise Exception("Could not get company name!")
 
         # Filters and gets the technical contacts associated with the company from airtable.
         contacts_url = contacts_url + f'?filterByFormula=AND(Company="{company_name}", SEARCH("{CONTACT_TYPE}",%7BContact Type%7D))'
@@ -91,15 +100,13 @@ class main(plugin_collection.Plugin):
             contacts_email.append(record["fields"].get("Email"))
 
         if not contacts_email:
-            print(f'No "{CONTACT_TYPE}" records found.')
-            contacts_email = {"Email": EMAIL_ADDRESS}
-            print(f"Email: {contacts_email}\n")
-        else:
-            contacts = {
-                "Company Name": company_name,
-                "Email": contacts_email
-            }
-            # print(json.dumps(contacts, indent=2))
+            raise Exception(f'No "{CONTACT_TYPE}" records found!')
+
+        contacts = {
+            "Company Name": company_name,
+            "Email": contacts_email
+        }
+        # print(json.dumps(contacts, indent=2))
 
         return(contacts)
 
@@ -109,7 +116,7 @@ class main(plugin_collection.Plugin):
         # print(json.dumps(airtable_response, indent=2))
         has_items = bool(airtable_response["records"])
         if not has_items:
-            print("Airtable responded with empty response!")
+            logging.error("Airtable responded with empty response!")
         return(airtable_response)
 
     def create_alert_log(self, node, node_name, network_name, alert_log_path, recipients_email):
@@ -167,7 +174,7 @@ class main(plugin_collection.Plugin):
             # 1: 2 hours/120 minutes. 2: 24 hours/1440 minutes. 3: 48 Hours/2880 minutes.
             if minutes >= time_till_email:
                 # Send email
-                email_sent = self.send_email(node_name, network_name, recipients_email, cc_email, html_content, plainText_content)
+                email_sent = self.send_email(node_name, network_name, plainText_content, recipients_email=recipients_email, cc_email=cc_email, html_content=html_content)
                 if email_sent:
                     # Open and update the alert with the time the email was sent.
                     data["notify"][stage]["time_sent"] = str(datetime.datetime.now().strftime('%s'))
@@ -185,7 +192,7 @@ class main(plugin_collection.Plugin):
         else:
             print(f'\033[91mAll emails have been sent to {node_name}.\033[m')
 
-    def send_email(self, node, network_name, recipients_email, cc_email, html_content, plainText_content):
+    def send_email(self, node_name, network_name, plainText_content, subject = None, recipients_email: str = None, cc_email: str = None, html_content: str = None):
         EMAIL_ADDRESS = os.environ.get('Sovrin_Email_App_User')
         EMAIL_PASSWORD = os.environ.get('Sorvin_Email_App_Pwd')
         EMAIL_RECIPIENT = None
@@ -200,7 +207,10 @@ class main(plugin_collection.Plugin):
             log_folder = 'net3'
 
         msg = EmailMessage()
-        msg['Subject'] = f'Your node ({node}) on the {network_name} needs attention'
+        if subject:
+            msg['Subject'] = subject
+        else:
+            msg['Subject'] = f'Your node ({node_name}) on the {network_name} needs attention'
         msg['From'] = EMAIL_ADDRESS
         msg['To'] = EMAIL_RECIPIENT
         msg['Cc'] = EMAIL_CC
@@ -208,12 +218,13 @@ class main(plugin_collection.Plugin):
         # Email will send a HTML and plain text options for email clients that don't support HTML format.
 
         # Plain Text Option #
-        content = open(plainText_content).read().format(node=node, network_name=network_name, log_folder=log_folder, recipients_email=recipients_email)
+        content = open(plainText_content).read().format(node=node_name, network_name=network_name, log_folder=log_folder, recipients_email=recipients_email)
         msg.set_content(content)
 
         # HTML Option #
-        html = open(html_content).read().format(node=node, network_name=network_name, log_folder=log_folder, recipients_email=recipients_email)
-        msg.add_alternative(html, subtype='html')
+        # if html_content:
+        #     html = open(html_content).read().format(node=node_name, network_name=network_name, log_folder=log_folder, recipients_email=recipients_email)
+        #     msg.add_alternative(html, subtype='html')
 
         # Send Email with contents and return if the email was sent.
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
@@ -222,3 +233,4 @@ class main(plugin_collection.Plugin):
             email_sent = True
         
         return email_sent
+
